9d75ce120e47a18f7f9eec4cf1f0981cfb1a2d86
w <hy-wu20@mails.tsinghua.edu.cn>
2024-11-19 21:37:21 +0800

    deploy

--- deploy.py
+++ deploy.py
@@ -1,18 +1,10 @@
-# npm run build
-# echo build
-# cd dist
-# echo cd_dist
-# git init
-# echo git_init
-# git add -A
-# git commit -m 'deploy'
-# git push -f git@github.com:hy-wu/shanxi-heritage-art.git main:gh-pages
-# cd -
+# 本地构建推送到远程仓库gh-pages分支
 import os
 os.system('npm run build')
-os.system('cd dist')
+os.chdir('dist')
+print(os.getcwd())
 os.system('git init')
 os.system('git add -A')
 os.system('git commit -m "deploy"')
 os.system('git push -f git@github.com:hy-wu/shanxi-heritage-art.git main:gh-pages')
-os.system('cd -')
+os.chdir('..')
